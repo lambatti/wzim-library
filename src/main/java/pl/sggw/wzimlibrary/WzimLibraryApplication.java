b09199c14d0b5faa48342156f795d6205369e8ac
lambatti <adamgrzegorzgala@gmail.com>
2022-03-06 18:01:52 +0100

    Initial commit

---
+++ src/main/java/pl/sggw/wzimlibrary/WzimLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.sggw.wzimlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzimLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzimLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sggw/wzimlibrary/WzimLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sggw.wzimlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzimLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
