7749f9e11a71ac44f4fb5933be27e41d9b646e2e
lambatti <adamgrzegorzgala@gmail.com>
2022-03-06 22:15:15 +0100

    Added SecurityConfig with h2-console permissions

---
+++ src/main/java/pl/sggw/wzimlibrary/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package pl.sggw.wzimlibrary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+}
