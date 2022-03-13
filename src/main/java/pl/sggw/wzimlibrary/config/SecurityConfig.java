5b6f8c278b6fc35bb3d3e847c2f146459dce3528
lambatti <adamgrzegorzgala@gmail.com>
2022-03-13 22:35:30 +0100

    Added LoginController, updated security config (permit all everywhere)

--- src/main/java/pl/sggw/wzimlibrary/config/SecurityConfig.java
+++ src/main/java/pl/sggw/wzimlibrary/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/").permitAll()
+                .authorizeRequests().antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
