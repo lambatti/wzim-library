4280fd1f885b21f851b60b320b897d8cbc7edecb
lambatti <adamgrzegorzgala@gmail.com>
2022-03-13 21:50:35 +0100

    Added ValidationLength class with constants

---
+++ src/main/java/pl/sggw/wzimlibrary/model/ValidationLength.java
@@ -0,0 +1,9 @@
+package pl.sggw.wzimlibrary.model;
+
+public final class ValidationLength {
+    public final static int FIRST_NAME_MAX_LENGTH = 20;
+    public final static int LAST_NAME_MAX_LENGTH = 40;
+    public final static int PASSWORD_MIN_LENGTH = 7;
+    public final static int PASSWORD_MAX_LENGTH = 30;
+    public final static int SECURITY_QUESTION_ANSWER_MAX_LENGTH = 30;
+}
