323d5b59812dd8cf9ec5064521fac4a22a1847cf
lambatti <adamgrzegorzgala@gmail.com>
2022-03-13 20:50:36 +0100

    Added example users migration, fixed security question varchar size

--- src/main/resources/db/migration/V1__init_users_table.sql
+++ src/main/resources/db/migration/V1__init_users_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     email                       VARCHAR(255) NOT NULL,
     password                    VARCHAR(30) NOT NULL,
     gender                      CHAR(1) NOT NULL,
-    security_question           VARCHAR(10) NOT NULL,
+    security_question           VARCHAR(15) NOT NULL,
     security_question_answer    VARCHAR(30) NOT NULL,
     role                        VARCHAR(10) NOT NULL
 )
