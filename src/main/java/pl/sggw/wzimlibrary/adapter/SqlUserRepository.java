32427ac2b4bf9b4efd1ba2e6c65a8129ed0a185a
lambatti <adamgrzegorzgala@gmail.com>
2022-03-13 21:28:19 +0100

    Added findByEmail method

--- src/main/java/pl/sggw/wzimlibrary/adapter/SqlUserRepository.java
+++ src/main/java/pl/sggw/wzimlibrary/adapter/SqlUserRepository.java
@@ -6,10 +6,12 @@ import pl.sggw.wzimlibrary.model.User;
 import pl.sggw.wzimlibrary.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SqlUserRepository extends UserRepository, JpaRepository<User, Integer> {
 
     @Override
     List<User> findAll();
 
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/pl/sggw/wzimlibrary/repository/UserRepository.java
+++ src/main/java/pl/sggw/wzimlibrary/repository/UserRepository.java
@@ -3,7 +3,11 @@ package pl.sggw.wzimlibrary.repository;
 import pl.sggw.wzimlibrary.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
+
     List<User> findAll();
+
+    Optional<User> findByEmail();
 }
