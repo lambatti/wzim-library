698c9682fcd52e09712eaafd519eb34178f4d444
lambatti <adamgrzegorzgala@gmail.com>
2022-03-13 22:34:29 +0100

    Updated migrations to include bcrypt passwords

--- src/main/resources/db/migration/V1__init_users_table.sql
+++ src/main/resources/db/migration/V1__init_users_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     first_name                  VARCHAR(20) NOT NULL,
     last_name                   VARCHAR(40) NOT NULL,
     email                       VARCHAR(255) NOT NULL,
-    password                    VARCHAR(30) NOT NULL,
+    password                    VARCHAR(60) NOT NULL,
     gender                      CHAR(1) NOT NULL,
     security_question           VARCHAR(15) NOT NULL,
     security_question_answer    VARCHAR(30) NOT NULL,
--- src/main/resources/db/migration/V2__insert_example_users.sql
+++ src/main/resources/db/migration/V2__insert_example_users.sql
@@ -1,4 +1,7 @@
 INSERT INTO users (first_name, last_name, email, password, gender, security_question, security_question_answer, role)
-VALUES ('Adam', 'Gała', 'adam@gmail.com', 'adamszef', 'M', 'ANIMAL', 'kitku', 'USER'),
-       ('Kamil', 'Gryciuk', 'kamil@wp.pl', 'kamilez', 'M', 'SANDWICH', 'kurczak', 'WORKER'),
-       ('Tomasz', 'Comasz', 'tomasz@coma.sz', 'szefito', 'F', 'FRIENDS_NAME', 'chlebek', 'ADMIN')
+VALUES ('Adam', 'Gała', 'adam@gmail.com', '$2a$12$cRBDwcTunnQV8S4Fl54noOiV7EHHt5yLN3FudkdSH/pNac9Jtpe/C', 'M', 'ANIMAL',
+        'kitku', 'USER'),
+       ('Kamil', 'Gryciuk', 'kamil@wp.pl', '$2a$12$3d3s7R2m52iduyOVA4MRteW2i2a.WdlAQ6CVKesZdR.FHKuQDRR.6', 'M',
+        'SANDWICH', 'kurczak', 'WORKER'),
+       ('Tomasz', 'Comasz', 'tomasz@coma.sz', '$2a$12$yMlOMyr6nDsvKezXyZSNde4TqLuL5vkk0cAYqc3VDo7D6DZNisbg2', 'F',
+        'FRIENDS_NAME', 'chlebek', 'ADMIN')
