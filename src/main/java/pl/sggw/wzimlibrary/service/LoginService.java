2908be4159827871d47c650b3a1e252ecee1d7ef
lambatti <adamgrzegorzgala@gmail.com>
2022-03-13 22:24:58 +0100

    Added LoginService

---
+++ src/main/java/pl/sggw/wzimlibrary/service/LoginService.java
@@ -0,0 +1,39 @@
+package pl.sggw.wzimlibrary.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import pl.sggw.wzimlibrary.model.authentication.AuthenticationRequest;
+import pl.sggw.wzimlibrary.model.authentication.AuthenticationResponse;
+import pl.sggw.wzimlibrary.util.JwtUtil;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public Optional<AuthenticationResponse> generateToken(AuthenticationRequest authenticationRequest) {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(),
+                            authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            return Optional.empty();
+        }
+
+        UserDetails userDetails = userService.loadUserByUsername(authenticationRequest.getEmail());
+        String jwt = jwtUtil.generateToken(userDetails);
+        return Optional.of(new AuthenticationResponse(jwt));
+    }
+
+}
