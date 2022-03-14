16204d3fc44bff82bd38cb40e02220b6da6b0153
lambatti <adamgrzegorzgala@gmail.com>
2022-03-14 16:29:33 +0100

    Added @EnableAsync to the main file

--- src/main/java/pl/sggw/wzimlibrary/WzimLibraryApplication.java
+++ src/main/java/pl/sggw/wzimlibrary/WzimLibraryApplication.java
@@ -2,12 +2,14 @@ package pl.sggw.wzimlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class WzimLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WzimLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WzimLibraryApplication.class, args);
+    }
 
 }
