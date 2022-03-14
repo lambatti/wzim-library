9d034203395208d9cee54e1c908df55e5138ede6
lambatti <adamgrzegorzgala@gmail.com>
2022-03-14 12:52:59 +0100

    Fixed password in example users

--- src/main/resources/db/migration/V2__insert_example_users.sql
+++ src/main/resources/db/migration/V2__insert_example_users.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (first_name, last_name, email, password, gender, security_question, security_question_answer, role)
 VALUES ('Adam', 'Gała', 'adam@gmail.com', '$2a$12$cRBDwcTunnQV8S4Fl54noOiV7EHHt5yLN3FudkdSH/pNac9Jtpe/C', 'M', 'ANIMAL',
         'kitku', 'USER'),
-       ('Kamil', 'Gryciuk', 'kamil@wp.pl', '$2a$12$3d3s7R2m52iduyOVA4MRteW2i2a.WdlAQ6CVKesZdR.FHKuQDRR.6', 'M',
+       ('Kamil', 'Gryciuk', 'kamil@wp.pl', '$2a$12$IUn.qT8ABVAyODOC5Lnj/.37G.Xh41GYNgHnCLkKTO8S9gImcT2nK', 'M',
         'SANDWICH', 'kurczak', 'WORKER'),
        ('Tomasz', 'Comasz', 'tomasz@coma.sz', '$2a$12$yMlOMyr6nDsvKezXyZSNde4TqLuL5vkk0cAYqc3VDo7D6DZNisbg2', 'F',
         'FRIENDS_NAME', 'chlebek', 'ADMIN')
