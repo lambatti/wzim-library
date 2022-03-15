74856ec196b5dad29432d66aa95bad844d77031d
lambatti <adamgrzegorzgala@gmail.com>
2022-03-15 12:16:03 +0100

    Added UNIQUE to email column

--- src/main/resources/db/migration/V1__init_users_table.sql
+++ src/main/resources/db/migration/V1__init_users_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     id                          SERIAL PRIMARY KEY,
     first_name                  VARCHAR(20) NOT NULL,
     last_name                   VARCHAR(40) NOT NULL,
-    email                       VARCHAR(255) NOT NULL,
+    email                       VARCHAR(255) NOT NULL UNIQUE,
     password                    VARCHAR(60) NOT NULL,
     gender                      CHAR(1) NOT NULL,
     security_question           VARCHAR(15) NOT NULL,
