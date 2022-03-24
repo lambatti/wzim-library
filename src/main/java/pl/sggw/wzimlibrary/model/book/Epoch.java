c83cf6becb17439e10d1f19dcc6a6effce225b6c
gstfnk <gabrysia.stefaniak@gmail.com>
2022-03-24 19:49:19 +0100

    Add Epoch prop

---
+++ src/main/java/pl/sggw/wzimlibrary/model/book/Epoch.java
@@ -0,0 +1,34 @@
+
+package pl.sggw.wzimlibrary.model.book;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name"
+})
+@Generated("jsonschema2pojo")
+public class Epoch {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
