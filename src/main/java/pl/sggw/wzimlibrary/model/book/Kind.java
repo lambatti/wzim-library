ab11103aefc7085d37a5c0c98cd0e3c196e0ebd6
gstfnk <gabrysia.stefaniak@gmail.com>
2022-03-24 19:49:07 +0100

    Add Kind prop

---
+++ src/main/java/pl/sggw/wzimlibrary/model/book/Kind.java
@@ -0,0 +1,28 @@
+
+package pl.sggw.wzimlibrary.model.book;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "name"
+})
+public class Kind {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
