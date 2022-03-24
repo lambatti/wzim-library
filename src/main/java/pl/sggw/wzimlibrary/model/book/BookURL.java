1c6c85d87b8e891f164921bcef443e326839034c
gstfnk <gabrysia.stefaniak@gmail.com>
2022-03-24 19:49:45 +0100

    Add BookURL Model

---
+++ src/main/java/pl/sggw/wzimlibrary/model/book/BookURL.java
@@ -0,0 +1,25 @@
+package pl.sggw.wzimlibrary.model.book;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "slug"
+})
+
+public class BookURL {
+    @JsonProperty("slug")
+    private String slug;
+
+    @JsonProperty("slug")
+    public String getSlug() {
+        return slug;
+    }
+
+    @JsonProperty("slug")
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+}
