f26555bed98cbe686d165920aba3233903ca3154
lambatti <adamgrzegorzgala@gmail.com>
2022-04-01 19:21:40 +0200

    Added constants for scheduling

---
+++ src/main/java/pl/sggw/wzimlibrary/model/constant/SchedulingConstant.java
@@ -0,0 +1,6 @@
+package pl.sggw.wzimlibrary.model.constant;
+
+public final class SchedulingConstant {
+    public final static String EVERY_DAY_AT_MIDNIGHT = "0 0 0 * * *";
+    public final static String TIMEZONE = "Europe/Warsaw";
+}
