27c59c7ebee8be306490db4fab141e0982f1a03d
Kamil <k-gryciuk@wp.pl>
2022-04-09 22:05:03 +0200

    Added routing and logout action to the header

--- src/main/resources/frontend/src/app/core/components/header/header.component.ts
+++ src/main/resources/frontend/src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { AuthService } from '../../authentication/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,11 +13,16 @@ export class HeaderComponent {
   @Input() isRegister: boolean = false;
   public name: string = 'Tomek';
 
-  constructor(public _auth: AuthService) {
+  constructor(public _auth: AuthService, private router: Router) {
   }
 
   get isAuth(): boolean {
     return this._auth.isAuthenticated();
+  }
+
+  logout(): void {
+    this._auth.logout();
+    this.router.navigateByUrl('/');
   }
 
 
