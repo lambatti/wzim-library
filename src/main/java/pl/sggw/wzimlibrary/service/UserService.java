2df33c1b46be19b6250eef88fd5fc08b3ab4bd5c
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-05-01 17:34:17 +0200

    Comments removed

--- src/main/java/pl/sggw/wzimlibrary/service/UserService.java
+++ src/main/java/pl/sggw/wzimlibrary/service/UserService.java
@@ -54,8 +54,6 @@ public class UserService implements UserDetailsService {
 
     @Async
     public CompletableFuture<Void> setPassword(String email, String encodedPassword) {
-//        return CompletableFuture.completedFuture(userCacheService.setPassword(email, encodedPassword)).complete(null);
-//        return CompletableFuture.completedFuture(null);
         userCacheService.setPassword(email, encodedPassword);
         return null;
     }
@@ -208,7 +206,6 @@ public class UserService implements UserDetailsService {
 
     private void setUserPassword(User user, String newPassword) {
         String encodedPassword = passwordEncoder.encode(newPassword);
-        //userCacheService.setPassword(user.getEmail(), encodedPassword);
         setPassword(user.getEmail(), encodedPassword);
     }
 
