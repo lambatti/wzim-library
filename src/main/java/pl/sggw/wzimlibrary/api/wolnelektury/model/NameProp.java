0ecdfff6f9a7bed84ee14b199409be07ffc8a1b3
gstfnk <gabrysia.stefaniak@gmail.com>
2022-05-07 23:02:28 +0200

    add lombock to NameProp

--- src/main/java/pl/sggw/wzimlibrary/api/wolnelektury/model/NameProp.java
+++ src/main/java/pl/sggw/wzimlibrary/api/wolnelektury/model/NameProp.java
@@ -2,29 +2,17 @@
 package pl.sggw.wzimlibrary.api.wolnelektury.model;
 
 
-import javax.annotation.Generated;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-        "name"
-})
+@Getter
+@Setter
 public class NameProp {
     @JsonProperty("name")
     private String name;
-
-    @JsonProperty("name")
-    public String getName() {
-        return name;
-    }
-
-    @JsonProperty("name")
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public String toString() {
