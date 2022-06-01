f5a72f5d75ed413b95999f6a0e17f04e9676eecd
Kamil <k-gryciuk@wp.pl>
2022-06-01 21:43:16 +0200

    Changed login controller token is in a body

--- src/main/java/pl/sggw/wzimlibrary/controller/LoginController.java
+++ src/main/java/pl/sggw/wzimlibrary/controller/LoginController.java
@@ -1,7 +1,6 @@
 package pl.sggw.wzimlibrary.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.sggw.wzimlibrary.exception.UserNotFoundException;
@@ -18,10 +17,23 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    private static class LoginResponse {
+        private final String token;
+
+        public LoginResponse(String token) {
+            this.token = token;
+        }
+
+        public String getToken() {
+            return token;
+        }
+    }
+
     @PostMapping("login")
     ResponseEntity<?> createAuthenticationToken(@Valid @RequestBody AuthenticationRequest authenticationRequest) throws UserNotFoundException {
+
+
         return ResponseEntity.ok()
-                .header(HttpHeaders.AUTHORIZATION, loginService.generateToken(authenticationRequest))
-                .build();
+                .body(new LoginResponse(loginService.generateToken(authenticationRequest)));
     }
 }
