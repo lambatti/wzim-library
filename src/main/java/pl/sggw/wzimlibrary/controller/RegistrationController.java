0ac0278c33df43788444a10262d4c78c203952b3
lambatti <adamgrzegorzgala@gmail.com>
2022-06-11 23:13:30 +0200

    Deleted mailing

--- src/main/java/pl/sggw/wzimlibrary/controller/RegistrationController.java
+++ src/main/java/pl/sggw/wzimlibrary/controller/RegistrationController.java
@@ -1,20 +1,15 @@
 package pl.sggw.wzimlibrary.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.sggw.wzimlibrary.exception.UserAlreadyExistsException;
-import pl.sggw.wzimlibrary.exception.UserDecryptionException;
 import pl.sggw.wzimlibrary.model.User;
 import pl.sggw.wzimlibrary.model.dto.user.UserRegistrationDto;
-import pl.sggw.wzimlibrary.service.RegistrationService;
 import pl.sggw.wzimlibrary.service.UserService;
 
-import javax.mail.MessagingException;
 import javax.validation.Valid;
-import java.io.IOException;
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
 
@@ -25,27 +20,6 @@ import java.util.concurrent.ExecutionException;
 public class RegistrationController {
 
     private final UserService userService;
-    private final RegistrationService registrationService;
-
-    @PostMapping("sendRegistrationMail")
-    public ResponseEntity<?> sendRegistrationMail(@Valid @RequestBody UserRegistrationDto userRegistrationDto)
-            throws MessagingException, JsonProcessingException {
-
-        registrationService.sendRegistrationMail(userRegistrationDto,
-                ServletUriComponentsBuilder.fromCurrentRequest().replacePath("/api").build().toUriString());
-
-        return ResponseEntity.ok().build();
-
-    }
-
-    @GetMapping("activateAccount")
-    public ResponseEntity<?> activateAccount(@RequestParam String user)
-            throws IOException, ExecutionException, InterruptedException, UserAlreadyExistsException,
-            UserDecryptionException {
-
-        return registerUser(registrationService.getUserFromEncryptedMessage(user));
-
-    }
 
     @PostMapping("registerUser")
     ResponseEntity<?> registerUser(@Valid @RequestBody UserRegistrationDto userRegistrationDto)
